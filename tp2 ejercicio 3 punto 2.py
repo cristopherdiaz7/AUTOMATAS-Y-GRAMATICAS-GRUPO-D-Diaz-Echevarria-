001677a1fa3bc00f396628e2ebd37299e73ac74b
cristopherdiaz7 <crl.diaz@alumno.um.edu.ar>
2025-05-30 20:07:42 -0300

    EJERCICIOS ARREGLADOS

--- tp2 ejercicio 3 punto 2.py
+++ tp2 ejercicio 3 punto 2.py
@@ -1,39 +1,54 @@
-AFN = {
-    0: {'a': [0], 'b': [0], 'ε': [1]},
-    1: {'a': [2], 'b': [2], 'ε': [2]},
-    2: {}
-}
 
-estados_finales = {2}
-estado_inicial = 0
+from collections import defaultdict
 
-def epsilon_closure(estados):
-    stack = list(estados)
-    closure = set(estados)
-    while stack:
-        estado = stack.pop()
-        for destino in AFN.get(estado, {}).get('ε', []):
-            if destino not in closure:
-                closure.add(destino)
-                stack.append(destino)
-    return closure
+transiciones = defaultdict(lambda: defaultdict(list))
+
+transiciones[0]['ε'] = [1, 6]
+transiciones[1]['ε'] = [2, 4]
+transiciones[2]['a'] = [3]
+transiciones[4]['b'] = [5]
+transiciones[3]['ε'] = [7]
+transiciones[5]['ε'] = [7]
+transiciones[6]['ε'] = [1]
+transiciones[7]['ε'] = [6, 8]
+
+transiciones[8]['a'] = [9]
+transiciones[8]['b'] = [10]
+transiciones[8]['ε'] = [11]
+transiciones[9]['ε'] = [11]
+transiciones[10]['ε'] = [11]
+
+inicio = 0
+final = 11
+
+def cerradura_epsilon(estados):
+    pila = list(estados)
+    resultado = set(estados)
+    while pila:
+        estado = pila.pop()
+        for destino in transiciones[estado].get('ε', []):
+            if destino not in resultado:
+                resultado.add(destino)
+                pila.append(destino)
+    return resultado
 
 def mover(estados, simbolo):
     resultado = set()
     for estado in estados:
-        resultado.update(AFN.get(estado, {}).get(simbolo, []))
+        for destino in transiciones[estado].get(simbolo, []):
+            resultado.add(destino)
     return resultado
 
 def acepta_afn(cadena):
-    actuales = epsilon_closure({estado_inicial})
+    actuales = cerradura_epsilon({inicio})
     for simbolo in cadena:
-        siguientes = mover(actuales, simbolo)
-        actuales = epsilon_closure(siguientes)
-    return any(estado in estados_finales for estado in actuales)
-
+        actuales = cerradura_epsilon(mover(actuales, simbolo))
+    return final in actuales
 
 print(acepta_afn("a"))
-print(acepta_afn("abba"))
+print(acepta_afn("bb"))
 print(acepta_afn(""))
-print(acepta_afn("ababx"))
+print(acepta_afn("abab"))
+print(acepta_afn("c"))
+
 
--- tp2 ejercicio 3 punto 4.py
+++ tp2 ejercicio 3 punto 4.py
@@ -1,21 +1,25 @@
-AFD = {
-    'A': {'a': 'A', 'b': 'A'}
+transicion_afd = {
+    0: {'a': 1, 'b': 2},
+    1: {'a': 1, 'b': 2},
+    2: {'a': 1, 'b': 2}
 }
 
-estado_inicial = 'A'
-estado_final = {'A'}
+estado_inicial = 0
+estados_finales = {1, 2}
 
 def acepta_afd(cadena):
-    estado_actual = estado_inicial
+    estado = estado_inicial
     for simbolo in cadena:
-        if simbolo not in AFD[estado_actual]:
+        if simbolo in transicion_afd[estado]:
+            estado = transicion_afd[estado][simbolo]
+        else:
             return False
-        estado_actual = AFD[estado_actual][simbolo]
-    return estado_actual in estado_final
+    return estado in estados_finales
 
-print(acepta_afd(""))
 print(acepta_afd("a"))
-print(acepta_afd("bbba"))
-print(acepta_afd("babab"))
+print(acepta_afd("bb"))
+print(acepta_afd(""))
+print(acepta_afd("abab"))
 print(acepta_afd("c"))
+
 
